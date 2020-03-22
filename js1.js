1906ab1ffb122a5082587041f96a0aab808d4ac0
LoliaStarGirl <46056002+LoliaStarGirl@users.noreply.github.com>
2020-03-22 14:50:36 +0100

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,21 @@
+<script>
+let name = "Lolia";
+ console.log("My name is: " + name);
+
+let course = ["HTML" , "CSS" , "Javascript"];
+  console.log("My courses are : " + course);
+
+const info = [];
+
+ for (let i=1; i<=200; i++) {
+
+   if (i%2 != 0) {
+
+     info.push(i);
+
+   }
+
+ }
+
+ console.log(info);
+</script>
